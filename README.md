6f0bf031e571d9d24301a718a137fd4de8a48556
Ultramarine-spec <951917080@qq.com>
2025-03-07 12:02:31 +0800

    add

---
+++ README.md
@@ -0,0 +1,4 @@
+# LR<sup>2</sup>Bench: Evaluating Long-chain Reflective Reasoning Capabilities of Large Language Models via Constraint Satisfaction Problems
+<p align="center">
+    📖 <a href="https://arxiv.org/abs/2502.17848" target="_blank">Paper</a> • 🤗 <a href="https://huggingface.co/spaces/UltraRonin/LR2Bench" target="_blank">Leaderboard</a>
+</p>
---
+++ gen.sh
@@ -0,0 +1,136 @@
+# export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export CUDA_VISIBLE_DEVICES=$1
+export VLLM_LOGGING_LEVEL="ERROR"
+# nohup bash scripts/gen.sh > ./1.log 2>&1 &
+
+task=$2
+models=$3
+
+
+if [ $task == "0-crossword" ]; then
+    cd ./data/$task
+    python prompt.py
+    cd ../..
+    for model in $models; do
+        for setting in 5_5 10_10 15_15; do
+            task_path=./data/$task/$setting
+            prompt_name="prompt.json"
+            max_tokens=32768
+
+            python generate.py \
+                --model $model \
+                --task_path $task_path \
+                --prompt_name $prompt_name \
+                --temperature 0.0 \
+                --top_p 1.0 \
+                --max_tokens $max_tokens \
+                --vllm \
+                --drop_zero_shot
+        done
+    done
+elif [ $task == "1-acrostic" ]; then
+    cd ./data/$task
+    python prompt.py
+    cd ../..
+    for model in $models; do
+        for setting in easy hard; do
+            task_path=./data/$task/$setting
+            prompt_name="prompt.json"
+            max_tokens=32768
+
+            python generate.py \
+                --model $model \
+                --task_path $task_path \
+                --prompt_name $prompt_name \
+                --temperature 0.0 \
+                --top_p 1.0 \
+                --max_tokens $max_tokens \
+                --vllm \
+                --drop_zero_shot
+        done
+    done
+elif [ $task == "2-logic" ]; then
+    cd ./data/$task
+    python prompt.py
+    cd ../..
+    for model in $models; do
+        for setting in 4_4 4_5 4_6 4_7; do
+            task_path=./data/$task/$setting
+            prompt_name="prompt.json"
+            max_tokens=32768
+
+            python generate.py \
+                --model $model \
+                --task_path $task_path \
+                --prompt_name $prompt_name \
+                --temperature 0.0 \
+                --top_p 1.0 \
+                --max_tokens $max_tokens \
+                --vllm \
+                --drop_zero_shot
+        done
+    done
+elif [ $task == "3-cryptogram" ]; then
+    cd ./data/$task
+    python prompt.py
+    cd ../..
+    for model in $models; do
+        for setting in easy hard; do
+            task_path=./data/$task/$setting
+            prompt_name="prompt.json"
+            max_tokens=32768
+
+            python generate.py \
+                --model $model \
+                --task_path $task_path \
+                --prompt_name $prompt_name \
+                --temperature 0.0 \
+                --top_p 1.0 \
+                --max_tokens $max_tokens \
+                --vllm \
+                --drop_zero_shot
+        done
+    done
+elif [ $task == "4-sudoku" ]; then
+    cd ./data/$task
+    python prompt.py
+    cd ../..
+    for model in $models; do
+        for setting in 4_4_easy 4_4_hard 9_9_easy 9_9_hard; do
+            task_path=./data/$task/$setting
+            prompt_name="prompt.json"
+            max_tokens=32768
+
+            python generate.py \
+                --model $model \
+                --task_path $task_path \
+                --prompt_name $prompt_name \
+                --temperature 0.0 \
+                --top_p 1.0 \
+                --max_tokens $max_tokens \
+                --vllm \
+                --drop_zero_shot
+        done
+    done
+elif [ $task == "5-drop" ]; then
+    cd ./data/$task
+    python prompt.py
+    cd ../..
+    for model in $models; do
+        for setting in easy hard; do
+            task_path=./data/$task/$setting
+            prompt_name="prompt.json"
+            max_tokens=32768
+
+            python generate.py \
+                --model $model \
+                --task_path $task_path \
+                --prompt_name $prompt_name \
+                --temperature 0.0 \
+                --top_p 1.0 \
+                --max_tokens $max_tokens \
+                --vllm \
+                --drop_zero_shot
+        done
+    done
+fi
